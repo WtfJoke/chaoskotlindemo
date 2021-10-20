867c2bf9f54d2b1b60c01ac86535f1893251b01c
Manuel <wtfjoke@livenet.ch>
2021-10-20 22:16:24 +0200

    :arrow_up: Bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.4.1"
-    id("io.spring.dependency-management") version "1.0.10.RELEASE"
-    id("org.jetbrains.kotlin.plugin.noarg") version "1.4.10"
-    id("org.jetbrains.kotlin.plugin.jpa") version "1.4.10"
-    kotlin("jvm") version "1.4.21"
-    kotlin("plugin.spring") version "1.4.21"
+    id("org.springframework.boot") version "2.5.5"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.5.31"
+    kotlin("plugin.spring") version "1.5.31"
+    kotlin("plugin.jpa") version "1.5.31"
 }
 
 group = "com.example"
@@ -25,9 +24,11 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("io.github.resilience4j:resilience4j-core:1.6.1")
-    implementation("de.codecentric:chaos-monkey-spring-boot:2.4.6-SNAPSHOT")
-    implementation("no.finn.unleash:unleash-client-java:4.3.0")
+
+    implementation("io.github.resilience4j:resilience4j-core:1.7.0")
+    implementation("de.codecentric:chaos-monkey-spring-boot:2.5.4")
+    implementation("no.finn.unleash:unleash-client-java:4.4.0")
+
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
